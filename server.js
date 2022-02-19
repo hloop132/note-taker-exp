c5c3a45b6d0ed763b87ac1902338907ded7e22fe
heather loop <hloop132@gmail.com>
2022-02-18 20:14:53 -0800

    wrote to db

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 
 const PORT = 3001;
 
@@ -10,27 +10,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
-let notesArr = fs.readFileSync("./db/db.json");
-console.log(typeof(notesArr));
+const notesArr = JSON.parse(fs.readFileSync("./db/db.json"));
+console.log(typeof notesArr);
 
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
-);
-
-app.get("/*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 app.get("/api/notes", (req, res) => {
   return res.json(notesArr);
 });
 
-app.post("/api/notes", (req,res) => {
-   let newNote = req.body 
-   notesArr.push(newNote)
-   res.send();
+app.post("/api/notes", (req, res) => {
+  const note = req.body;
+  notesArr.push(note);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notesArr));
+  res.send();
 });
 
+app.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
 app.listen(PORT, () =>
   console.log("app be listenin on http://localhost:${PORT}")
