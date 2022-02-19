f62e6f582e042cd40b2fb89feb708b0cbf9fdf09
heather loop <hloop132@gmail.com>
2022-02-18 21:57:26 -0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # note-taker-exp
+
+## Description
+A note taker
+
+## Instruction
+Type in the note title and the note you want and save
+
+## Installation
+npm i for express
+
+## Technologies
+html, css, javascript, node , express
+
+## Contributions
+My school UW and me for not giving up even though I feel like it all the time
+
+## Demo
+ ![](./public/assets/note-taker.gif)
+
+ ## Licence 
+ none
--- server.js
+++ server.js
@@ -1,26 +1,34 @@
+//requiring express, fs, path
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
+//claiming the port
 const PORT = 3001;
 
+//claiming app as express
 const app = express();
 
+//encodng something just not sure what
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//calling the static files public \
 app.use(express.static("public"));
 const notesArr = JSON.parse(fs.readFileSync("./db/db.json"));
 console.log(typeof notesArr);
 
+//joining the path/route of notes to public notes
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+//creating the api to get all the entries
 app.get("/api/notes", (req, res) => {
   return res.json(notesArr);
 });
 
+//getting all the notes written and oushing them to the db.json file
 app.post("/api/notes", (req, res) => {
   const note = req.body;
   notesArr.push(note);
@@ -28,10 +36,12 @@ app.post("/api/notes", (req, res) => {
   res.send();
 });
 
+//joining the html to the server 
 app.get("/*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
+//creatting a port and where to listen 
 app.listen(PORT, () =>
   console.log("app be listenin on http://localhost:${PORT}")
 );
